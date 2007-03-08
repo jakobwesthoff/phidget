2b0eea2701070ffc4542e1d6963d10658d4a3772
jakob <jakob@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-03-08 21:42:23 +0000

    - Added simple moz embed test
    git-svn-id: svn://localhost/phidget@4 ca0deab5-1f2b-0410-8023-f33ecadb3f17

---
+++ src/mozembed.php
@@ -0,0 +1,35 @@
+<?php
+
+    $window = new GtkWindow();
+
+    // Connect destroy event
+    $window->connect_simple( 'destroy', array( 'gtk', 'main_quit' ) );
+
+    // Remove the window borders
+    $window->set_decorated( false );
+
+    // Hide the window from the taskbar an the pager
+    $window->set_skip_pager_hint( true );
+    $window->set_skip_taskbar_hint( true );
+
+    // Display it on every virtual desktop
+    $window->stick();
+
+    // Set its size and center it on screen
+    $screen = Gdk::get_default_root_window()->get_screen();
+    $window->resize( $screen->get_width(), $screen->get_width() );
+    $window->set_position( Gtk::WIN_POS_CENTER );
+
+    // The window should be always below all others
+    $window->set_keep_below( true );
+    $window->set_type_hint( Gdk::WINDOW_TYPE_HINT_DOCK );
+
+    // Add mozembed
+    $moz = new GtkMozEmbed();
+    $window->add( $moz );
+    $moz->load_url( 'http://google.de' );
+
+    // Show the window
+    $window->show_all();
+
+?>
