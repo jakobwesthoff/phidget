acecaa358459a2cc864cd6ce4911625870192ac3
jakob <jakob@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-03-16 02:51:58 +0000

    - Splitted starterbar and starterbar item classes in two files - Moved the starterbar it's own subfolder # Now the cleanup can start
    git-svn-id: svn://localhost/phidget@15 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/autoload/widget_autoload.php
+++ src/autoload/widget_autoload.php
@@ -1,10 +1,12 @@
 <?php
 
 return array( 
-    'jdWidget'           =>  'classes/widget.php',    
+    'jdWidget'                      =>  'classes/widget.php',    
 
-    'jdWidgetExample'    =>  'widgets/example/example.php',
-    "jdWidgetStarterbar" =>  "widgets/starterbar.php"
+    'jdWidgetExample'               =>  'widgets/example/example.php',
+
+    'jdWidgetStarterbar'            =>  'widgets/starterbar/starterbar.php',
+    'jdWidgetStarterbarIcon'        =>  'widgets/starterbar/icon.php',
 );
 
 ?>
--- src/widgets/starterbar/icon.php
+++ src/widgets/starterbar/icon.php
@@ -1,113 +1,6 @@
 <?php
-/**
- *
- * @author Manuel Pichler <pichler@i-world.de>
- * @version 0.1
- */
-class jdWidgetStarterbar extends jdWidget
-{
 
-    /**
-     * Temporary all icons for the starterbar. This should be 
-     * configurable.
-     *
-     * @type array<string>
-     * @var array $_icons
-     */
-    private $_icons = array(
-        "pixmaps/terminal.png", 
-        "pixmaps/web-browser.png", 
-        "pixmaps/help.png" );
-
-    /**
-     * <tt> Array</tt> with all starterbar items.
-     *
-     * @type array<jdStarterbarItem>
-     * @var array $_items
-     */
-    private $_items = array();
-
-    /**
-     *
-     */
-    private $_width = 0;
-
-    public function __construct( $configuration ) {
-        parent::__construct( $configuration );
-
-        $offsetX = 0;
-        foreach ( $this->_icons as $icon ) {
-            $this->_items[] = new jdStarterbarItem( $icon, $offsetX, 64 );
-            
-            $offsetX += 80;
-        } 
-
-        $this->add_events( Gdk::BUTTON_PRESS_MASK |
-                           Gdk::POINTER_MOTION_MASK );
-        $this->connect( "button-press-event", array( $this, "OnMousePress" ) );
-        $this->connect( "motion-notify-event", array( $this, "OnMouseMove" ) );
-
-    }
-
-    protected function getSize()
-    {
-        return array( 50 + sizeof( $this->_icons ) * 80, 150 );
-    }
-
-    public function OnExpose( $gc, $window )
-    {
-        $cmap = $window->get_colormap();
-        $size = $this->getSize();
-
-        $gc->set_foreground( $cmap->alloc_color( "#000000" ) );
-        $window->draw_rectangle( $gc, false, 0, 0, $size[0] - 1, 149 );
-
-        foreach ( $this->_items as $item ) {
-            $item->OnExpose( $gc, $window );
-        }
-    }
-
-    public function OnMousePress(jdWidget $source, GdkEvent $event) {
-        print "CLICK ME\n";
-    }
-
-    private $_random = 0;
-
-    private $_eventX = 0;
-    private $_eventY = 0;
-
-    public function OnMouseMove(jdWidget $source, GdkEvent $event) {
-        $scale = 0.0;
-        $index = 0;
-
-        if ( $event->x === $this->_eventX && $event->y === $this->_eventY ) {
-            return;
-        }
-        $this->_eventX = $event->x;
-        $this->_eventY = $event->y;
-
-//        if ( $this->_random++ % 30 !== 0 ) { return; }
-        
-        $index = -1;
-        $scale = 0.0;
-
-        foreach ( $this->_items as $i => $item ) {
-            if ( ( $s = $item->scaleForEvent( $event ) ) > 0.2 ) {
-                // Ask for redraw
-                $source->window->invalidate_rect(
-		    new GdkRectangle(
-                        $item->OffsetX, 0, $item->Width, $item->Height), false );
-
-                $scale = $s;
-                $index = $i;
-
-                break;
-            }
-        }
-    }
-}
-
-class jdStarterbarItem {
+class jdWidgetStarterbarIcon {
 
     const MAX_SCALE = 0.7;
 
@@ -214,3 +107,5 @@ class jdStarterbarItem {
     }
 
 }
+
+?>
