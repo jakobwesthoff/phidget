ccd3cae6f2596653859a7c653fa4e2b63c2a3644
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-03-17 21:56:15 +0000

    New class structure added.
    git-svn-id: svn://localhost/phidget@36 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/autoload/widget_autoload.php
+++ src/autoload/widget_autoload.php
@@ -9,7 +9,14 @@ return array(
     'jdWidgetStarterbarIcon'        =>  'widgets/starterbar/icon.php',
 
     'jdWidgetFinder'                =>  'widgets/finder/finder.php',
-    'jdWidgetFinderIcon'            =>  'widgets/finder/icon.php',
+    'jdWidgetFinderAnimation'       =>  'widgets/finder/anim.php',
+    'jdWidgetFinderAnimationJump'   =>  'widgets/finder/anim/jump.php',
+    'jdWidgetFinderBackground'      =>  'widgets/finder/bg.php',
+    'jdWidgetFinderCommand'         =>  'widgets/finder/command.php',
+    'jdWidgetFinderItem'            =>  'widgets/finder/item.php',
+    'jdWidgetFinderClockItem'       =>  'widgets/finder/item/clock.php',
+    'jdWidgetFinderIconItem'        =>  'widgets/finder/item/icon.php',
+    'jdWidgetFinderSeparator'       =>  'widgets/finder/item/separator.php',
 );
 
 ?>
