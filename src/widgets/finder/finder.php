db9266f2a56ae1159debef8bdaa5454ad3f37c8d
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-03-17 21:51:13 +0000

    Some extensions to scaling added. # Some scaling and moving changes added. # This version contains y offset scaling.
    Sorry jakob if I override some of you extensions ;) But I believe I have all your primary changes.
    
    git-svn-id: svn://localhost/phidget@34 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/widgets/finder/finder.php
+++ src/widgets/finder/finder.php
@@ -9,13 +9,15 @@
  */
 class jdWidgetFinder extends jdWidget
 {
+    protected $background;
     protected $icons;
+    protected $size = null;
 
     protected $lastMouseX;
     protected $lastMouseY;
 
-    public function __construct( $configuration ) {
-        // Constructor of the superclass
+    public function __construct( SimpleXMLElement $configuration )
+    {
         parent::__construct( $configuration );
         
         // Initialize all class members
@@ -23,49 +25,83 @@ class jdWidgetFinder extends jdWidget
         $this->lastMouseX = 0;
         $this->lastMouseY = 0;
         
-        // Initialize all icons
-        $xoffset = round( $this->configuration->size / 2.0 );
-        foreach ( $this->configuration->icons->icon as $filename ) 
-        {
-            $icon = new jdWidgetFinderIcon( (string) $filename );           
-            $icon->size = (int) $this->configuration->size;
-            $this->icons[] = $icon;
-            echo "added Icon: ", ( string ) $filename, "\n";
-        } 
+        // Make local properties
+        $icozoom  = (int) $this->configuration->zoom;
+        $icosize  = (int) $this->configuration->size;
+        $icospace = (int) $this->configuration->space;
+                
+        // Calculate x and y offsets
+        $xoffset = $icosize + ( $icospace * ( ( $icozoom - $icosize ) / ( $icospace * 2 ) ) );
+        $yoffset = $icozoom - round( $icosize / 2.0 );
 
-        $size = $this->getSize();
-        // Beginning xoffset left based (NOT CENTER BASED)
-        $xoffset = round( ( $size[0] - ( count( $this->icons ) * (int) $this->configuration->size + ( count( $this->icons ) - 1 ) * (int) $this->configuration->space ) ) / 2.0 );
-        // Correct the x,y positions of the icons
-        foreach( $this->icons as $icon ) 
+        foreach ( $this->configuration->items->item as $itemconfig ) 
         {
-            $icon->x = $xoffset + round( $icon->size / 2.0 );
-            $icon->y = $size[1] - round( $icon->size / 2.0 );
-            $xoffset += $icon->size + (int) $this->configuration->space;
-        }
+            // Ask factory for new item instance
+	        $item = jdWidgetFinderItem::createItem(
+	                                        $this->configuration, 
+	                                        $itemconfig,
+	                                        $xoffset,
+	                                        $yoffset );
+	                                        
+            // Check for none standard item width
+	        if ( $item->size !== $icosize ) 
+	        {
+	            // Reset item x offset
+	            $item->x = $item->x + ( ( $item->size - $icosize ) / 2 );
+	        }
+
+            // Calculate next x offset
+            $xoffset += $item->size + $icospace;
+            
+            $this->icons[] = $item;
+        } 
 
         // Connect to the needed signals
         $this->add_events( 
             Gdk::BUTTON_PRESS_MASK
            |Gdk::POINTER_MOTION_MASK
+           |Gdk::LEAVE_NOTIFY_MASK
         );
         $this->connect( "button-press-event", array( $this, "OnMousePress" ) );
         $this->connect( "motion-notify-event", array( $this, "OnMouseMove" ) );
+        $this->connect( "leave-notify-event", array( $this, "OnMouseLeave" ) );
+        
+        $size = $this->getSize();
+        
+        $this->background = new jdWidgetFinderBackground( 
+                                    (string) $this->configuration->background,
+                                    $size[0], 
+                                    $size[1],
+                                    $icosize
+                                );
     }
 
     protected function getSize()
     {
-        //@todo: change size calculation function, to reflect the real size of the bar. This one is not accurate
-        //       anymore because of the zoomoffset config value
+        // Check for previous size calculation
+        if ( $this->size !== null )
+        {
+            return $this->size;
+        }
+        
+        // Sum width of all items 
+        $itemwidth = 0;
+	    foreach ( $this->icons as $item )
+	    {
+	        $itemwidth += $item->size;
+	    }
+        
         /* Needed width is calculated as follows:
-         * ( number of icons ) * ( icon size ) + ( ( number of icons ) - 1 ) * ( space between icons ) + ( maximum icon size )
+         * sum( icon size ) + ( ( number of icons ) - 1 ) * ( space between icons ) + ( maximum icon size )
          * Needed height is calculated as follows:
          * At the moment this is just the maximum icon size
          */
-        return array(
-            count( $this->icons ) * (int) $this->configuration->size + ( count( $this->icons ) - 1 ) * (int) $this->configuration->space + (int) $this->configuration->zoom,
+        $this->size = array(
+            $itemwidth + ( count( $this->icons ) - 1 ) * (int) $this->configuration->space + (int) $this->configuration->zoom,
             (int) $this->configuration->zoom
         );
+        
+        return $this->size;
     }
 
     public function OnExpose( $gc, $window )
@@ -77,6 +113,8 @@ class jdWidgetFinder extends jdWidget
         $gc->set_foreground( $cmap->alloc_color( "#000000" ) );
         $window->draw_rectangle( $gc, false, 0, 0, $size[0] - 1, $size[1] - 1 );
         
+        $this->background->draw( $gc, $window );
+        
         // Draw every icon to the widget surface
         foreach( $this->icons as $icon ) 
         {
@@ -86,10 +124,36 @@ class jdWidgetFinder extends jdWidget
     }
 
     public function OnMousePress( jdWidget $source, GdkEvent $event ) 
-    {
-        //@todo: Determine which icon has been clicked. This should be easy by retrieving
-        //       the biggest icon.
-        echo "clicked!\n";
+    {   
+        // Difference between event x and widget x
+        $diff = PHP_INT_MAX;
+        // The clicked icon instance
+        $clickedIcon = null;
+
+        // Find the matching icon
+        foreach ( $this->icons as $icon ) 
+        {
+            $tmp = abs( $event->x - $icon->x );
+            if ( $tmp < $diff ) 
+            {
+                $diff = $tmp;
+                $clickedIcon = $icon;
+            }
+        }
+        
+        if ( $clickedIcon !== null ) 
+        {
+            // Check for left button click
+            if ( $event->button === 1 )
+            {
+                $clickedIcon->doLeftClick( $source->window );
+	        } 
+	        else if ( $event->button === 3 ) 
+	        {
+	            $clickedIcon->doRightClick( $source->window );
+	        }
+//            new jdWidgetFinderIconAction( $clickedIcon, $source->window );
+        }
     }
 
     public function OnMouseMove( jdWidget $source, GdkEvent $event ) 
@@ -105,23 +169,23 @@ class jdWidgetFinder extends jdWidget
         $this->lastMouseY = $event->y;
 
         $size = $this->getSize();
-
-        $xoffset = round( ( $size[0] - ( count( $this->icons ) * (int) $this->configuration->size + ( count( $this->icons ) - 1 ) * (int) $this->configuration->space ) ) / 2.0 ) + round( $this->configuration->size / 2.0 );
-        $mouseX = $event->x;
         
+        // Calculate the center of the first icon
+        $xoffset = round( $this->configuration->zoom * 0.75 );
+
         $realwidth = 0;
         // Scale all icons
-        // xoffset is center based
+        // xoffset is center base
         foreach ( $this->icons as $icon )
         {
-            $scalefactor = $this->calculateScaling( $xoffset, $mouseX );
-            // Calculate the new size and y position
+            $scalefactor = $this->calculateScaling( $xoffset, $event->x, $event->y ); // Calculate the new size and y position
             $icon->size = (int) $this->configuration->size * $scalefactor;
-            $icon->y = $size[1] - round( $icon->size / 2.0 );
+            $icon->y    = (int) $this->configuration->zoom - round( $icon->size / 2.0 ) - ( 1.5 * $scalefactor );
 
-            $xoffset += (int) $this->configuration->size + (int) $this->configuration->space;
-            $realwidth += $icon->size + (int) $this->configuration->space;            
+            $xoffset   += (int) $icon->size + (int) $this->configuration->space;
+            $realwidth += $icon->size + (int) $this->configuration->space;
         }
+        
         $realwidth -= (int) $this->configuration->space;
 
         // Calc new xoffset based on the real width of the bar
@@ -133,9 +197,9 @@ class jdWidgetFinder extends jdWidget
             $icon->x = $xoffset + round( $icon->size / 2.0 );
             $xoffset += $icon->size + (int) $this->configuration->space;
         }
-
         // @todo: check if redraw is really neccessary
         // Redraw the widget
+        $size = $this->getSize();
         $source->window->invalidate_rect(
             new GdkRectangle(
                 0,
@@ -146,16 +210,41 @@ class jdWidgetFinder extends jdWidget
             false
         );
     }
-
-    protected function calculateScaling( $center, $mouseX ) 
+    
+    public function OnMouseLeave( jdWidget $source, GdkEvent $event ) 
     {
-        $multiplier = (int) $this->configuration->zoomoffset - abs( $center - $mouseX );
+        // Delegete leave event to mouse move, it contains the logic.
+        $this->OnMouseMove( $source, $event );
+    }
+
+    protected function calculateScaling( $center, $mouseX, $mouseY ) 
+    {
+        
+        $size = $this->getSize();
+        
+        if ( $mouseX <= 0 
+          || $mouseY <= 0
+          || $mouseX >= $size[0]
+          || $mouseY >= $size[1] ) 
+        {
+            // No scaling here, the mouse left the widget.
+            return 1.0;
+        }
+        
+        $multiplierX = (int) $this->configuration->zoomoffset - abs( $center - $mouseX );
+        $multiplierY = (int) $this->configuration->zoomoffset - abs( $this->configuration->zoom - $mouseY );
+        $multiplier = round( ( ( $multiplierX * (int) $this->configuration->zoom ) + $multiplierY ) / (int) $this->configuration->zoom );
+
         if ( $multiplier < 0 ) 
         {
             return 1.0;
         }
-        $scalefactor = ( 1.0 + ( ( (float) $multiplier * ( ( (float) $this->configuration->zoom - (float) $this->configuration->size ) / (float) $this->configuration->zoomoffset ) ) / (float) $this->configuration->size ) );
+        
+        //@todo: something is definetly wrong with the scaling factor calculation
+        //       I will fix this tommorow I am just to tired now
+        $scalefactor = ( ( (float) $multiplier * ( ( (float) $this->configuration->zoom - (float) $this->configuration->size ) / (float) $this->configuration->zoomoffset ) ) / (float) $this->configuration->size );
+        $scalefactor = ( 1.0 + ( ( $scalefactor / $this->configuration->zoomoffset) * $multiplierY ) );
+        
         return $scalefactor;
     }
 }
-
