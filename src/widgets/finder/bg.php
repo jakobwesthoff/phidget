6033a033386975f32e72ddd23e04ea42cda476f4
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-03-17 21:38:14 +0000

    Background class added. It allows custom finder background handling.
    git-svn-id: svn://localhost/phidget@32 ca0deab5-1f2b-0410-8023-f33ecadb3f17

---
+++ src/widgets/finder/bg.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * jdWidgetFinderBackground
+ * 
+ * @version //autogen//
+ * @copyright Copyright (C) 2007 Jakob Westhoff. All rights reserved.
+ * @author Manuel Pichler <mp@manuel-pichler.de>
+ * @license GPL
+ */
+class jdWidgetFinderBackground {
+    
+    protected $properties = array();
+    
+    public function __construct( $filename, $width, $height, $size )
+    {
+        $this->properties = array(
+            "pixbuf" => GdkPixbuf::new_from_file( $filename ),
+            "width"  => $width,
+            "height" => $height,
+            "size"   => $size);
+    }
+    
+    public function draw( GdkGC $gc, GdkWindow $window )
+    {
+        
+        $cmap = $window->get_colormap();
+        
+        // Calculate x/y offsets and width/height
+        $scaled  = round( (int) $this->size * 0.9 );
+        $offsetY = $this->height -  $this->size;
+        $width   = $this->width - ( 2 * $scaled );
+
+        // Create a item border
+        $gc->set_foreground( $cmap->alloc_color( "#cccccc" ) );
+        $window->draw_pixbuf( $gc, $this->pixbuf, 0, 0, $scaled, $offsetY, $width - 1, $scaled );
+        $window->draw_rectangle( $gc, false, $scaled, $offsetY, $width - 1, $scaled );
+    }
+    
+    /**
+     * Overloaded function to retrieve the available properties
+     * (Default behaviour: Everything not explicitedly denied will be allowed)
+     *
+     * @param mixed $key Property to retrieve
+     * @return void
+     */
+    public function __get( $key )
+    {
+        switch( $key )
+        {
+            default:
+                if ( !array_key_exists( $key, $this->properties ) )
+                {
+                    throw new jdBasePropertyException( $key, jdBasePropertyException::READ );
+                }
+                return $this->properties[$key];
+        }
+    }
+
+    /**
+     * Overloaded function to set properties
+     * (Default behaviour: Everything not explicitly allowed will be denied)
+     *
+     * @param mixed $key Property to set
+     * @param mixed $val Value to set for property
+     * @return void
+     */
+    public function __set( $key, $val )
+    {
+        switch( $key )
+        {
+            case 'width':
+            case 'height':
+            case 'size':
+                $this->properties[$key] = $val;
+                break;
+
+            default:
+                throw new jdBasePropertyException( $key, jdBasePropertyException::WRITE );
+        }
+    }
+}
