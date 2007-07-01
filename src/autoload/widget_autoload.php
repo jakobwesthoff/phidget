bbf7210e0d373208a643bbda93d66454a33ba823
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-07-01 13:01:17 +0000

    - New classes added to autoload file.
    git-svn-id: svn://localhost/phidget@79 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/autoload/widget_autoload.php
+++ src/autoload/widget_autoload.php
@@ -1,23 +1,26 @@
 <?php
 
-return array( 
-    'jdWidget'                      =>  'classes/widget.php',    
+return array(
+    'jdWidget'                        =>  'classes/widget.php',
 
-    'jdWidgetExample'               =>  'widgets/example/example.php',
+    'jdWidgetExample'                 =>  'widgets/example/example.php',
 
-    'jdWidgetStarterbar'            =>  'widgets/starterbar/starterbar.php',
-    'jdWidgetStarterbarIcon'        =>  'widgets/starterbar/icon.php',
+    'jdWidgetStarterbar'              =>  'widgets/starterbar/starterbar.php',
+    'jdWidgetStarterbarIcon'          =>  'widgets/starterbar/icon.php',
 
-    'jdWidgetFinder'                =>  'widgets/finder/finder.php',
-    'jdWidgetFinderAnimation'       =>  'widgets/finder/anim.php',
-    'jdWidgetFinderAnimationJump'   =>  'widgets/finder/anim/jump.php',
-    'jdWidgetFinderBackground'      =>  'widgets/finder/bg.php',
-    'jdWidgetFinderCommand'         =>  'widgets/finder/command.php',
-    'jdWidgetFinderItem'            =>  'widgets/finder/item.php',
-    'jdWidgetFinderClockItem'       =>  'widgets/finder/item/clock.php',
-    'jdWidgetFinderIconItem'        =>  'widgets/finder/item/icon.php',
-    'jdWidgetFinderSeparatorItem'   =>  'widgets/finder/item/separator.php',
-    'jdWidgetFinderTrashItem'       =>  'widgets/finder/item/trash.php',
+    'jdWidgetFinder'                  =>  'widgets/finder/finder.php',
+    'jdWidgetFinderAnimation'         =>  'widgets/finder/anim.php',
+    'jdWidgetFinderAnimationJump'     =>  'widgets/finder/anim/jump.php',
+    'jdWidgetFinderBackground'        =>  'widgets/finder/bg.php',
+    'jdWidgetFinderCommand'           =>  'widgets/finder/command.php',
+    'jdWidgetFinderEffect'            =>  'widgets/finder/effect.php',
+    'jdWidgetFinderItem'              =>  'widgets/finder/item.php',
+    'jdWidgetFinderEffectScale'       =>  'widgets/finder/effect/scale.php',
+    'jdWidgetFinderEffectSizeStruct'  =>  'widgets/finder/effect/size_struct.php',
+    'jdWidgetFinderClockItem'         =>  'widgets/finder/item/clock.php',
+    'jdWidgetFinderIconItem'          =>  'widgets/finder/item/icon.php',
+    'jdWidgetFinderSeparatorItem'     =>  'widgets/finder/item/separator.php',
+    'jdWidgetFinderTrashItem'         =>  'widgets/finder/item/trash.php',
 );
 
 ?>
