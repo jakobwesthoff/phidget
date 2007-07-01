0b891e529e67fa7ba055ba9b98ddcf0481cb7d40
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-07-01 12:56:24 +0000

    - ctor signature changed, x/y offset parameters aren't required anymore.
    git-svn-id: svn://localhost/phidget@75 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/widgets/finder/item.php
+++ src/widgets/finder/item.php
@@ -5,59 +5,55 @@
  * @version //autogen//
  * @copyright Copyright (C) 2007 Jakob Westhoff, Manuel Pichler.
  *            All rights reserved.
- * @author Jakob Westhoff <jakob@php.net> 
+ * @author Jakob Westhoff <jakob@php.net>
  * @author Manuel Pichler <mapi@manuel-pichler.de>
  * @license GPL
  */
-abstract class jdWidgetFinderItem 
+abstract class jdWidgetFinderItem
 {
-    
+
     /**
-     * Factory method for the different finder items. 
+     * Factory method for the different finder items.
      *
      * @param SimpleXMLElement $widgetconf
      * @param SimpleXMLElement $iconconf
-     * @param integer $x
-     * @param integer $y
      */
-    public static function createItem( SimpleXMLElement $widgetconf, 
-                                       SimpleXMLElement $iconconf,
-                                       $x, 
-                                       $y )
+    public static function createItem( SimpleXMLElement $widgetconf,
+                                       SimpleXMLElement $iconconf )
     {
-        
-        // Split at all separator chars
-	    $type = preg_split( "#[-_\.]#", strtolower( (string) $iconconf->type ) );
 
-	    // Strip all empty elements
-	    $type = array_filter( $type );
-	    
-	    // If size is one, use default namespace
-	    if ( count( $type ) <= 1 )
-	    {
-	        array_unshift( $type, "widget" );
-	    }
-	    
-	    // No type given, use default "icon" 
-	    if ( count( $type ) === 1 )
-	    {
-	        array_push( $type, "icon" ); 
-	    }
-	    
-	    // Camel case all words
-	    $type = array_map( "ucfirst", $type );
-	    
-	    // Generate class name for item
-	    $class = "jd{$type[0]}Finder{$type[1]}Item";
-	    
-	    // Load item class and create instance
-	    $refClass = new ReflectionClass( $class );
-	    
-	    return $refClass->newInstanceArgs(
-	        array( $iconconf, $x, $y, (int) $widgetconf->size )
-	    );
+        // Split at all separator chars
+        $type = preg_split( "#[-_\.]#", strtolower( (string) $iconconf->type ) );
+
+        // Strip all empty elements
+        $type = array_filter( $type );
+
+        // If size is one, use default namespace
+        if ( count( $type ) <= 1 )
+        {
+            array_unshift( $type, "widget" );
+        }
+
+        // No type given, use default "icon"
+        if ( count( $type ) === 1 )
+        {
+            array_push( $type, "icon" );
+        }
+
+        // Camel case all words
+        $type = array_map( "ucfirst", $type );
+
+        // Generate class name for item
+        $class = "jd{$type[0]}Finder{$type[1]}Item";
+
+        // Load item class and create instance
+        $refClass = new ReflectionClass( $class );
+
+        return $refClass->newInstanceArgs(
+            array( $iconconf, (int) $widgetconf->size )
+        );
     }
-    
+
     /**
      * All properties for the item implementation.
      *
@@ -65,24 +61,21 @@ abstract class jdWidgetFinderItem
      * @var array $properties
      */
     protected $properties = array();
-    
+
     /**
-     * Constructor takes the configuration, the x/y offset and the item size as 
-     * argument.
-     * 
+     * Constructor takes the configuration and the item size as argument.
+     *
      * @param SimpleXMLElement $configuration
-     * @param integer $x
-     * @param integer $y
      * @param integer $size
      */
-    public function __construct( SimpleXMLElement $configuration, $x, $y, $size )
+    public function __construct( SimpleXMLElement $configuration, $size )
     {
         $this->properties = array(
             "configuration"  =>  $configuration,
             "locked"         =>  false,
             "size"           =>  $size,
-            "x"              =>  $x,
-            "y"              =>  $y
+            "x"              =>  0,
+            "y"              =>  0
         );
     }
 
@@ -129,15 +122,15 @@ abstract class jdWidgetFinderItem
                 throw new jdBasePropertyException( $key, jdBasePropertyException::WRITE );
         }
     }
-    
+
     /**
      * Empty template method for left mouse click.
      *
      * @param GdkWindow $window
      */
-    public function doLeftClick( GdkWindow $window ) 
+    public function doLeftClick( GdkWindow $window )
     {
-        
+
     }
 
     /**
