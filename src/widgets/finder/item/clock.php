00c3cc0aad04080aa4eadb04f63b7e20f35f2ca7
mapi <mapi@ca0deab5-1f2b-0410-8023-f33ecadb3f17>
2007-07-07 17:03:08 +0000

    Clock item modifications - Clock signature changed to match the new/old draw() method. - phpdoc comments completed for method and magic properties. - new, optional configuration attribute for seconds added. You can configure   clock with @seconds="false" to ignore seconds. If @seconds is "true" or is   not set this item will show seconds - refresh rate added. If this item doesn't show seconds a refresh rate of 60   seconds is enough.
    git-svn-id: svn://localhost/phidget@95 ca0deab5-1f2b-0410-8023-f33ecadb3f17

--- src/widgets/finder/item/clock.php
+++ src/widgets/finder/item/clock.php
@@ -2,6 +2,12 @@
 /**
  * jdWidgetFinderClockItem
  *
+ * @property-read GdkPixbuf $pixbuf The background image for the clock
+ * item.
+ * @property-read boolean $seconds Shall the clock show seconds?
+ * @property-read integer $refresh The widget refresh rate. If seconds
+ * are shown this will be a second otherwise we use a minute for refresh.
+ *
  * @version //autogen//
  * @copyright Copyright (C) 2007 Jakob Westhoff, Manuel Pichler.
  *            All rights reserved.
@@ -11,7 +17,13 @@
  */
 class jdWidgetFinderClockItem extends jdWidgetFinderItem
 {
-
+    /**
+     * The context window for this item. This window is used for item
+     * drawing.
+     *
+     * @type GdkWindow
+     * @var GdkWindow $window
+     */
     protected $window = null;
 
     /**
@@ -27,10 +39,33 @@ class jdWidgetFinderClockItem extends jdWidgetFinderItem
         // Set up icon item properties
         $this->properties["pixbuf"]  = GdkPixbuf::new_from_file( (string) $configuration->background );
 
+        // Check for a second setting
+        if ( isset( $configuration["seconds"] ) )
+        {
+            $this->properties["seconds"] = (string) $configuration["seconds"] === "true";
+        }
+        else
+        {
+            // No config, default is show seconds
+            $this->properties["seconds"] = true;
+        }
+
+        // If we don't show seconds, refresh rate is one minute
+        $this->properties["refresh"] = ( $this->seconds === true ? 1000 : 60000 );
+
         // TODO: Use a system property
         date_default_timezone_set( "Europe/Berlin" );
+
+        // Start initial timer
+        Gtk::timeout_add( $this->refresh, array( $this, "updateClock" ) );
     }
 
+    /**
+     * Main draw method for finder bar items.
+     *
+     * @param GdkGC $gc The currently used graphical context.
+     * @param GdkWindow $window The drawable context for the item.
+     */
     public function draw( GdkGC $gc, GdkWindow $window )
     {
         // Keep owner window
@@ -70,31 +105,56 @@ class jdWidgetFinderClockItem extends jdWidgetFinderItem
         $window->draw_line( $gc, $offsetX, $offsetY, $x, $y );
         $window->draw_line( $gc, $offsetX + 1, $offsetY + 1, $x, $y );
 
-        // New color for seconds
-        $color = $cmap->alloc_color( "#ff0000" );
-        $gc->set_foreground( $color );
+        // Check for seconds
+        if ( $this->seconds === true )
+        {
+            // New color for seconds
+            $color = $cmap->alloc_color( "#ff0000" );
+            $gc->set_foreground( $color );
 
-
-        // Draw seconds
-        list( $x, $y ) = $this->calculateXY( $s, ( $this->width * 0.8 ) );
-        $window->draw_line( $gc, $offsetX, $offsetY, $x, $y );
-
-        // Add gtk timer
-        Gtk::timeout_add( 1000, array( $this, "updateClock" ) );
+            // Draw seconds
+            list( $x, $y ) = $this->calculateXY( $s, ( $this->width * 0.8 ) );
+            $window->draw_line( $gc, $offsetX, $offsetY, $x, $y );
+        }
     }
 
+    /**
+     * Clock timer callback method. This method will request a redraw
+     * for the clock if this widget was drawn once.
+     */
     public function updateClock()
     {
-        $this->window->invalidate_rect(
-            new GdkRectangle(
-                $this->x,
-                $this->y,
-                $this->width,
-                $this->height
-             ), false
-        );
+        if ( $this->window !== null )
+        {
+            $this->window->invalidate_rect(
+                new GdkRectangle(
+                    $this->x,
+                    $this->y,
+                    $this->width,
+                    $this->height
+                 ), false
+            );
+        }
+
+        // Add gtk timer
+        Gtk::timeout_add( $this->refresh, array( $this, "updateClock" ) );
     }
 
+    /**
+     * Helper method for the clock it calculates the x/y offset for the
+     * pointers.
+     *
+     * <code>
+     *   array(
+     *     0  =>  x,  // The x offset
+     *     1  =>  y,  // The y offset
+     *   )
+     * </code>
+     *
+     * @param integer $time The time value
+     * @param integer $size The available pointer space.
+     * @return array The x/y offset.
+     */
     protected function calculateXY( $time, $size )
     {
         $b  = $size / 2;
@@ -107,5 +167,4 @@ class jdWidgetFinderClockItem extends jdWidgetFinderItem
             round( ( $this->y + ( $this->height * 0.5 ) ) - ( ( $b * sin( deg2rad( $ga ) ) ) / sin( deg2rad( $be ) ) ) )
         );
     }
-
 }
